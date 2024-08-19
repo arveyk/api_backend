0f2c5daa2fd7c9e566a6acc3f68c65b07ebfa19f
arveyk <harveykisiangani@gmail.com>
2024-08-20 01:36:50 +0300

    README.md

--- backend/server/app.py
+++ backend/server/app.py
@@ -27,12 +27,21 @@ def current_user():
     user_id = session.get("user_id")
     if not user_id:
         return jsonify({"error": "Unauthorized"}), 401
-    user = User.query.filter_by(id = user.id).first()
+    user = User.query.filter_by(id=user_id).first()
     return jsonify({
         "id": user.id,
         "email": user.email
         })
 
+@app.route("/users", methods=["GET"])
+def users_list():
+    """Retrives the current users from records
+    Args: None
+    Return: the  
+        response email and id
+    """
+    users = User.query.order_by(User.email).all()
+    return jsonify({"users": str(users)})
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -76,6 +85,32 @@ def logIn():
     return jsonify({
         "id": user.id,
         "email": user.email
+        })
+
+@app.route("/unregister", methods=["DELETE"])
+def delete_account():
+    """Removes in a user
+    Args:
+        request, with email and password params
+    Returns
+        User id
+    """
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email).first()
+    if user is None:
+        return jsonify({"error": "User does not exist"}), 404
+    if not bcrypt.check_password_hash(user._password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+    session["user_id"] = user.id
+    User.session.delete(user)
+    User.session.commit()
+    
+
+    return jsonify({
+        "email": email,
+        "status": "removed"
         })
 
 
